1b627107ac010bc16fed91579b5a348880ebe543
nara <parknara0213@gmail.com>
2024-09-09 11:50:46 +0900

    login 모델 생성

---
+++ src/models/login.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { mongo, Types } from "mongoose";
+
+const LoginSchema = new mongoose.Schema(
+  {
+    // user._id
+    user: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    //리프레시 토큰
+    refreshToken: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    // 장치 정보
+    device: {
+      type: String,
+      required: true,
+    },
+
+    // ip 주소
+    ip: {
+      type: String,
+      required: true,
+    },
+
+    // 로그인한 장소
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const Login = mongoose.model("Login", LoginSchema);
